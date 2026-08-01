91f98bcd376e212a13ce5d3f4d88668b927ffea9
嫣 <100054954+FairyXH@users.noreply.github.com>
2026-08-01 09:56:46 +0800

    Auto Push

--- 提交我的修改.bat
+++ 提交我的修改.bat
@@ -1,12 +1,13 @@
 @echo off
 cd /d "%~dp0"
-echo ======================
-echo 正在提交当前进度
-echo ======================
+echo 合并当前进度
+git pull
 
 git add .
 
-git commit -m "自动提交 %date% %time%"
+git commit -m "Auto Push"
+
+echo 正在提交当前进度
 
 git push
 
